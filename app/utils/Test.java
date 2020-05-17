a10077860156f91075d16663fe6eba3eb8a64a55
dvdanilin <denis@danilin.name>
2020-05-17 10:30:05 +0300

    Next gen. Only gui, nothing redundant

--- app/utils/Test.java
+++ app/utils/Test.java
@@ -4,16 +4,5 @@ import model.User;
 
 public class Test {
     public static void main(final String[] argz) throws Exception {
-        final User user = new User();
-        user.setOptions(141);
-
-        for (final UOpts o : UOpts.values()) {
-            System.out.println(o.name()+" >> " + o.is(user));
-//            if (TextUtils.rnd.nextInt() % 2 == 0) {
-                o.set(user);
-//                System.out.print(" apply '" + o.name() + ": " + user.getOptions());
-//            }
-//            System.out.print("; has ? " + o.is(user) + "\n");
-        }
     }
 }
