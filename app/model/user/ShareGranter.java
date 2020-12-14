5f4263cfc7515a83a2c3d5f7a38fe96bf4c4077b
dvdanilin <denis@danilin.name>
2020-12-14 13:45:41 +0300

    Experimental contacts search

--- app/model/user/ShareGranter.java
+++ app/model/user/ShareGranter.java
@@ -5,17 +5,25 @@ import model.Command;
 import model.ContentType;
 import model.TFile;
 import model.User;
+import play.Logger;
+import play.libs.Json;
 import services.TfsService;
 import services.TgApi;
 import services.UserService;
 import utils.LangMap;
+
+import java.util.concurrent.CompletableFuture;
+
+import static utils.TextUtils.*;
+import static utils.TextUtils.notNull;
 
 /**
  * @author Denis Danilin | denis@danilin.name
  * 24.06.2020
  * tfs ☭ sweat and blood
  */
-public class ShareGranter extends ARole implements CallbackSink {
+public class ShareGranter extends ARole implements CallbackSink, InputSink {
+    private static final Logger.ALogger logger = Logger.of(ShareGranter.class);
 
     public ShareGranter(final TgApi api, final TfsService tfs, final UserService us, final JsonNode node) {
         super(api, tfs, us, node);
@@ -40,6 +48,25 @@ public class ShareGranter extends ARole implements CallbackSink {
     public void doView() {
         final TFile entry = tfs.get(entryId, user);
         api.dialog(entry.isDir() ? LangMap.Value.SEND_CONTACT_DIR : LangMap.Value.SEND_CONTACT_FILE, user, entry.getName());
+    }
+
+    public void onInput(final String input) {
+        if (!isEmpty(input)) {
+            final long id = getLong(input);
+
+            if (id > 0 && input.matches("[0-9]+")) {
+                final TFile file = new TFile();
+                file.type = ContentType.CONTACT;
+                file.name = "u" + id;
+                file.setOwner(id);
+
+                onFile(file);
+                return;
+            } else
+                logger.info("Unknown input: " + input);
+        }
+
+        super.doSearch(input);
     }
 
     @Override
